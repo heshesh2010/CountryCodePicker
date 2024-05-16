4700dc5d9c9289dba7dd36d81bee1a0a8879b556
Hesham Elnemr <hesham.elnemr@gmail.com>
2024-05-16 04:49:32 +0300

    test padding

--- .history/lib/country_code_picker_20240516044851.dart
+++ .history/lib/country_code_picker_20240516044851.dart
@@ -164,55 +164,59 @@ class CountryCodePickerState extends State<CountryCodePicker> {
     else {
       _widget = TextButton(
         onPressed: widget.enabled ? showCountryCodePickerDialog : null,
-        child: Flex(
-          direction: Axis.horizontal,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            if (widget.showFlagMain != null
-                ? widget.showFlagMain!
-                : widget.showFlag)
-              Flexible(
-                flex: widget.alignLeft ? 0 : 1,
-                fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
-                child: Container(
-                  clipBehavior:
-                      widget.flagDecoration == null ? Clip.none : Clip.hardEdge,
-                  decoration: widget.flagDecoration,
-                  padding: widget.padding,
-                  child: Image.asset(
-                    selectedItem!.flagUri!,
-                    package: 'country_code_picker',
-                    width: widget.flagWidth,
+        child: Padding(
+          padding: widget.padding,
+          child: Flex(
+            direction: Axis.horizontal,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              if (widget.showFlagMain != null
+                  ? widget.showFlagMain!
+                  : widget.showFlag)
+                Flexible(
+                  flex: widget.alignLeft ? 0 : 1,
+                  fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
+                  child: Container(
+                    clipBehavior: widget.flagDecoration == null
+                        ? Clip.none
+                        : Clip.hardEdge,
+                    decoration: widget.flagDecoration,
+                    padding: const EdgeInsets.only(right: 0.0, left: 0.0),
+                    child: Image.asset(
+                      selectedItem!.flagUri!,
+                      package: 'country_code_picker',
+                      width: widget.flagWidth,
+                    ),
                   ),
                 ),
-              ),
-            if (!widget.hideMainText)
-              Flexible(
-                fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
-                child: Text(
-                  widget.showOnlyCountryWhenClosed
-                      ? selectedItem!.toCountryStringOnly()
-                      : selectedItem.toString(),
-                  style:
-                      widget.textStyle ?? Theme.of(context).textTheme.bodyLarge,
-                  overflow: widget.textOverflow,
+              if (!widget.hideMainText)
+                Flexible(
+                  fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
+                  child: Text(
+                    widget.showOnlyCountryWhenClosed
+                        ? selectedItem!.toCountryStringOnly()
+                        : selectedItem.toString(),
+                    style: widget.textStyle ??
+                        Theme.of(context).textTheme.bodyLarge,
+                    overflow: widget.textOverflow,
+                  ),
                 ),
-              ),
-            if (widget.showDropDownButton)
-              Flexible(
-                flex: widget.alignLeft ? 0 : 1,
-                fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
-                child: Padding(
-                    padding: widget.alignLeft
-                        ? const EdgeInsets.only(right: 20.0, left: 4.0)
-                        : const EdgeInsets.only(right: 4.0),
-                    child: Icon(
-                      Icons.arrow_drop_down,
-                      color: Colors.grey,
-                      size: widget.flagWidth,
-                    )),
-              ),
-          ],
+              if (widget.showDropDownButton)
+                Flexible(
+                  flex: widget.alignLeft ? 0 : 1,
+                  fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
+                  child: Padding(
+                      padding: widget.alignLeft
+                          ? const EdgeInsets.only(right: 20.0, left: 4.0)
+                          : const EdgeInsets.only(right: 4.0),
+                      child: Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.grey,
+                        size: widget.flagWidth,
+                      )),
+                ),
+            ],
+          ),
         ),
       );
     }
